8c4989ec82f962c40602e1b269c2fef7721d3cca
wzyrr520 <wzyrr520@163.com>
2019-10-30 20:45:20 +0800

    zhongwenban

--- file/client.js
+++ file/client.js
@@ -12,21 +12,38 @@ function showPicked(input) {
     reader.readAsDataURL(input.files[0]);
 }
 
+String.format = function(src){
+    if (arguments.length == 0) return null;
+    var args = Array.prototype.slice.call(arguments, 1);
+    return src.replace(/\{(\d+)\}/g, function(m, i){
+        return args[i];
+    });
+};
+
 function analyze() {
     var uploadFiles = el('file-input').files;
-    if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
-
-    el('analyze-button').innerHTML = 'Analyzing...';
-    var xhr = new XMLHttpRequest();
-    var loc = window.location
-    xhr.open('POST', 'http://123.57.235.182:8080', true);
-    xhr.onerror = function() {alert (xhr.responseText);}
-    xhr.onload = function(e) {
-        if (this.readyState === 4) {
-            var response = JSON.parse(e.target.responseText);
-            el('result-label').innerHTML = `Result = ${response['result']}`;
-        }
-        el('analyze-button').innerHTML = 'Analyze';
+    if (uploadFiles.length != 1) 
+		alert('请选择一个文件!');
+	else {
+		el('analyze-button').innerHTML = '分析中...';
+		var xhr = new XMLHttpRequest();
+		var loc = window.location
+		xhr.open('POST', 'http://123.57.235.182:8080', true);
+		xhr.onerror = function() {alert ('网络错误！');}
+		xhr.onload = function(e) {
+			if (this.readyState === 4) {
+				var response = JSON.parse(e.target.responseText);
+				if(response.result == 'Normal')
+					var re = '正常';
+				else 
+					var re = '异常';
+				var template = "结果为：{0}，且概率为{1}";
+				var site = response.Probability;
+				var msg = String.format(template, re, site);
+				el('result-label').innerHTML = msg;
+			}
+			el('analyze-button').innerHTML = '分析';
+		}
     }
 
     var fileData = new FormData();
